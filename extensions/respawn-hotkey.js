83efcc058ca9fe17b15eeeebebe5249effca8484
parsehex <1779937+parsehex@users.noreply.github.com>
2022-02-06 14:36:41 -0500

    shift + (1-5) to respawn as ship

--- extensions/respawn-hotkey.js
+++ extensions/respawn-hotkey.js
@@ -1,20 +1,27 @@
 !(function () {
 	SWAM.on('keyup', (e) => {
 		const { keyCode, shiftKey } = e;
-		console.log(e);
+		// console.log(e);
 		if (game?.gameType === null) return;
-		if (keyCode !== 89) return;
 		if (!shiftKey) return;
 
-		const ship = Players.getMe().type; // works even if /spec ing
+		let ship = '';
+		if (keyCode === 49) ship = '1';
+		else if (keyCode === 50) ship = '2';
+		else if (keyCode === 51) ship = '3';
+		else if (keyCode === 52) ship = '4';
+		else if (keyCode === 53) ship = '5';
+		else return;
+
+		// const ship = Players.getMe().type; // works even if /spec ing
 
 		Network.sendCommand('respawn', ship + '');
 	});
 
 	SWAM.registerExtension({
-		name: 'Respawn Hotkey',
-		id: 'respawn-hotkey',
-		description: 'Quickly respawn with a hotkey (Shift-Y).',
+		name: 'Hotkeys',
+		id: 'hotkeys',
+		description: 'Add hotkeys.',
 		author: 'parsehex',
 		version: '1.0',
 	});
